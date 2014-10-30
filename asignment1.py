6d57185e946809c404430644e5110c64fb35f556
{{git.name}} <alex1412108@gmail.com>
2014-10-30 14:41:19 +0000

    finished and working with autolabeling using filename

--- asignment1.py
+++ asignment1.py
@@ -1,26 +1,27 @@
 import cv2
 import numpy as np
+np.set_printoptions(threshold=np.nan)
 from matplotlib import pyplot as plt
 
-img = cv2.imread('coins1.png')
+filename = 'coins1.png'
+filenamearray = filename.split('.')
 
-#kernel = np.ones((5,5),np.float32)/25
-kernel = np.matrix([[1,0],[0,-1]])
+img = cv2.imread(filename)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-print kernel
-dst = cv2.filter2D(img,-1,kernel)
-
-#plt.subplot(121),plt.imshow(img),plt.title('Original')
-#plt.xticks([]), plt.yticks([])
-#plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
-#plt.xticks([]), plt.yticks([])
-#plt.show()
+kernel = np.matrix([[-1,0,1],[-2,0,2],[-1,0,1]])
+kernel2 = np.matrix([[1,2,1],[0,0,0],[-1,-2,-1]])
 
 
-while True:
-  cv2.imshow('coins1', img)
-  key = cv2.waitKey(10)
-  if key > 0:
-    print key
-    if key==1048603: #escape key
-      exit(0)
+img_final1 = cv2.filter2D(img,-1,kernel) + cv2.filter2D(img,-1,-kernel)
+img_final2 = cv2.filter2D(img,-1,kernel2) + cv2.filter2D(img,-1,-kernel2)
+img_final3 = img_final1/2 + img_final2/2
+
+cv2.imshow(filenamearray[0] + '_sobel_horizontal', img_final1)
+cv2.imshow(filenamearray[0] + '_sobel_vertical', img_final2)
+cv2.imshow(filenamearray[0] + '_sobel', img_final3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+finalfilename = filenamearray[0] + '_sobel' + '.' + filenamearray[1]
+cv2.imwrite(finalfilename, img_final3)
