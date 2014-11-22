bb8524c83a04b41128801a90138608ecd25fbc7c
{{git.name}} <alex1412108@gmail.com>
2014-11-22 21:54:44 +0000

    implement threshold and history equalization

--- assignment2/dartboarddetect.py
+++ assignment2/dartboarddetect.py
@@ -14,7 +14,7 @@ def main():
     detect_cascade = cv2.CascadeClassifier('cascade_resources/dartcascade.xml')
 
     folder_input = 'testsamples'
-    folder_output = 'results/basicdetect'
+    folder_output = 'results/thresholddetect'
 
     for filename in listdir(folder_input):
 
@@ -25,19 +25,25 @@ def main():
         
         # Convert to grayscale
         im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        #im_equal = cv2.equalizeHist (im_gray)
+        ret, im_thresh = cv2.threshold(im_gray,200,255,cv2.THRESH_BINARY)
 
-        detect = detect_cascade.detectMultiScale(im_gray, 1.1, 1, cv2.cv.CV_HAAR_SCALE_IMAGE, (50, 50), (500,500) )
-        detect_center=[]
+        detect = detect_cascade.detectMultiScale(im_thresh, 1.1, 1, cv2.cv.CV_HAAR_SCALE_IMAGE, (50, 50), (500,500) )
+
         for (x,y,w,h) in detect:
-            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+            detect_center=[]
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,255),2)
             #find center point
             detect_center.append(x+(w/2))
             detect_center.append(y+(w/2))
-            cv2.circle(im,(detect_center[0],detect_center[1]),2,(255,0,0),2)
+            cv2.circle(im,(detect_center[0],detect_center[1]),w/2,(0,255,255),2)
 
         finalfilename = filenamearray[0] + '_basic_detect' + '.' + filenamearray[1]
         # Display the image
         cv2.imwrite( folder_output + '/' + finalfilename, im )
-        #key = cv2.waitKey(0)
+
+    im_temp = cv2.imread(folder_output + '/' + 'dart0_basic_detect.jpg')
+    cv2.imshow( "Image", im_temp )
+    key = cv2.waitKey(0)
 
 main()
