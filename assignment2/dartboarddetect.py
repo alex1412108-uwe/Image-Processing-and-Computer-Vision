320a8d3fe67850570d973fe9268e83324a9d15fc
{{git.name}} <alex1412108@gmail.com>
2014-11-22 20:48:49 +0000

    working basic detect

--- assignment2/dartboarddetect.py
+++ assignment2/dartboarddetect.py
@@ -2,7 +2,8 @@ import time
 import argparse
 import cv2
 import numpy as np
-
+from os import listdir
+from os.path import isfile, join
 
 #---------------------------------------------------------------------------------------------------        
 #if __name__ == "__main__":
@@ -12,37 +13,31 @@ def main():
     #select cascade library
     detect_cascade = cv2.CascadeClassifier('cascade_resources/dartcascade.xml')
 
-    #name of image
-    filename = 'testsamples/dart0.jpg'
-    #array to store details of the image
-    filenamearray = filename.split('.')
+    folder_input = 'testsamples'
+    folder_output = 'results/basicdetect'
 
-    im = cv2.imread(filename) #gets image and greyscales it
-    
-    # Convert to grayscale
-    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    for filename in listdir(folder_input):
 
-    detect = detect_cascade.detectMultiScale(im_gray, 1.1, 1, cv2.cv.CV_HAAR_SCALE_IMAGE, (50, 50), (500,500) )
-    print detect
-    detect_center=[]
-    for (x,y,w,h) in detect:
-        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
-        #find center point
-        detect_center.append(x+(w/2))
-        detect_center.append(y+(w/2))
-        cv2.circle(im,(detect_center[0],detect_center[1]),2,(255,0,0),2)
-    
-    #find image dimensions
-    im_width, im_height = im_gray.shape[:2]
+        #array to store details of the image
+        filenamearray = filename.split('.')
 
-    # Display the image
-    cv2.imshow( "Image", im )
+        im = cv2.imread(folder_input + '/' + filename) #gets image
+        
+        # Convert to grayscale
+        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-    #check if user presses a key
-    key = cv2.waitKey(0)
-    if key > 0:
-        #print key
-        if key == 1048603:
-            exit(0)
+        detect = detect_cascade.detectMultiScale(im_gray, 1.1, 1, cv2.cv.CV_HAAR_SCALE_IMAGE, (50, 50), (500,500) )
+        detect_center=[]
+        for (x,y,w,h) in detect:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+            #find center point
+            detect_center.append(x+(w/2))
+            detect_center.append(y+(w/2))
+            cv2.circle(im,(detect_center[0],detect_center[1]),2,(255,0,0),2)
+
+        finalfilename = filenamearray[0] + '_basic_detect' + '.' + filenamearray[1]
+        # Display the image
+        cv2.imwrite( folder_output + '/' + finalfilename, im )
+        #key = cv2.waitKey(0)
 
 main()
