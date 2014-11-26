0880b567f36049342fbcebe7f6381d23404716f0
{{git.name}} <alex1412108@gmail.com>
2014-11-26 20:48:40 +0000

    working on range converting function

--- assignment2/dartboarddetect.py
+++ assignment2/dartboarddetect.py
@@ -14,7 +14,7 @@ def main():
     #select cascade library
     detect_cascade = cv2.CascadeClassifier('cascade_resources/dartcascade.xml')
 
-    folder_input = 'testsamples'
+    folder_input = 'testsamples2'
     folder_output = 'results/thresholddetect'
 
     for filename in listdir(folder_input):
@@ -34,6 +34,7 @@ def main():
         #sobel and then circle
         laplacian = cv2.Laplacian(im_gray,cv2.CV_64F)
         
+
         #detects the location of the object and saves it into a list
         detect = detect_cascade.detectMultiScale(im_gray, 1.1, 1, cv2.cv.CV_HAAR_SCALE_IMAGE, (50, 50), (500,500) )
 
@@ -50,15 +51,39 @@ def main():
         finalfilename = filenamearray[0] + '_basic_detect' + '.' + filenamearray[1]
         # saves the image
         cv2.imwrite( folder_output + '/' + finalfilename, im )
-        #cv2.imshow( "Image", im)
-        # cv2.imshow( "thresh", im_thresh )
-        plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-        plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+        #cv2.imshow( "Image", np.uint8(laplacian))
+        print laplacian
+        converted = np.zeros_like(laplacian)
+        numbermax = 0
+        numbermin = 0
+        for i in range(0,len(laplacian)):
+            for j in range(0,len(laplacian[0])):
+                if laplacian[i][j] > numbermax:
+                    numbermax = laplacian[i][j]
+                if laplacian[i][j] < numbermin:
+                    numbermin = laplacian[i][j]
+                converted[i][j] = convert_range(laplacian[i][j])
+        print numbermax, numbermin
+        print np.uint8(laplacian)
+
+        cv2.imshow( "thresh", converted )
+        #plt.subplot(1,1,1),plt.imshow(laplacian,cmap = 'gray')
+        #plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+        #plt.show()
         key = cv2.waitKey(0)
 
     #displays one of the saved images
     #im_temp = cv2.imread(folder_output + '/' + 'dart0_basic_detect.jpg')
     #cv2.imshow( "Image", im_temp )
     #key = cv2.waitKey(0)
+
+def convert_range(old_value, old_max = 1024, old_min = -1024, new_max = 255, new_min = 0):
+    OldRange = (old_max - old_min)
+    if OldRange == 0:
+        NewValue = new_min
+    else:
+        NewRange = (new_max - new_min)  
+        NewValue = (((old_value - old_min) * NewRange) / OldRange) + new_min
+    return NewValue
 
 main()
