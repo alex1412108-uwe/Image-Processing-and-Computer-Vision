89e5b7d4b3b16e35b79f5aedd94c29fbc513def6
{{git.name}} <alex1412108@gmail.com>
2014-12-15 00:22:19 +0000

    update comments and remove old commented out code, implemented finding regions of interest from a set of coordinates

--- assignment3/imagederivatives.py
+++ assignment3/imagederivatives.py
@@ -13,44 +13,29 @@ def main():
 	#video_source = cv2.VideoCapture(0)
 	#ret = video_source.set(3,1920) #set width
 	#ret = video_source.set(4,1080) #set height
+	regions_to_detect = [[200,200],[100,300],[50,50]]
+	region_size = [5,5]
 
 	im = setup(video_source)
-
 	im = cleanup(im)
 
 	while True:
 		im, im_past = running_cleanup(video_source, im)
 
-		array_ix, array_iy, array_it = find_values3(im, im_past)
+		region_list = regions(im, regions_to_detect, region_size)
 
-		#plt.imshow(array_ix, cmap = 'gray', interpolation = 'bicubic')
-		#plt.xticks([]), plt.yticks([])
-		#plt.show()
+		#array_ix, array_iy, array_it = find_values3(im, im_past)
 
-		# plt.subplot(2,2,1),plt.imshow(im,cmap = 'gray')
-		# plt.title('image'), plt.xticks([]), plt.yticks([])
-
-		# plt.subplot(2,2,2),plt.imshow(im_past,cmap = 'gray')
-		# plt.title('past'), plt.xticks([]), plt.yticks([])
-
-		# plt.subplot(2,2,3),plt.imshow(array_ix,cmap = 'gray')
-		# plt.title('ix'), plt.xticks([]), plt.yticks([])
-
-		# plt.subplot(2,2,4),plt.imshow(array_iy,cmap = 'gray')
-		# plt.title('iy'), plt.xticks([]), plt.yticks([])
-
-		# plt.show()
-
-		cv2.imshow('image', im)
-		cv2.imshow('image past', im_past)
-		cv2.imshow('ix', array_ix)
-		cv2.imshow('iy', array_iy)
-		cv2.imshow('it', array_it)
-		t = 0
-		# while t<1:
+		#cv2.imshow('image', im)
+		#cv2.imshow('image past', im_past)
+		#cv2.imshow('ix', array_ix)
+		#cv2.imshow('iy', array_iy)
+		#cv2.imshow('it', array_it)
+		exit(0)
 		key = cv2.waitKey(10)
 		if key > 0:
 			if key==1048603: #escape key
+				print region_list
 				exit(0)
 		# 		if key==1048608: #space key
 		# 			t = 2
@@ -105,9 +90,7 @@ def find_values2(im, im_past):
 	array_it = np.zeros((rows,cols), np.uint8)
 
 	for i in xrange(0,rows-1):
-
-		#line_it = []
-		for j in range(0,cols-1):
+		for j in xrange(0,cols-1):
 			array_spacial = im[i][j],im[i][j+1],im[i+1][j],im[i+1][j+1]
 			array_spacial_past = im_past[i][j],im_past[i][j+1],im_past[i+1][j],im_past[i+1][j+1]
 
@@ -116,6 +99,7 @@ def find_values2(im, im_past):
 			array_it[i][j] = ((array_spacial[0] - array_spacial_past[0])+(array_spacial[1] - array_spacial_past[1])+(array_spacial[2] - array_spacial_past[2])+(array_spacial[3] - array_spacial_past[3]))/4
 	return array_ix, array_iy, array_it
 
+#utilizes itemset to reduce computation power required
 def find_values3(im, im_past):
 	rows, cols = im.shape
 	array_spacial = []
@@ -128,7 +112,7 @@ def find_values3(im, im_past):
 	for i in xrange(0,rows-1):
 
 		#line_it = []
-		for j in range(0,cols-1):
+		for j in xrange(0,cols-1):
 			array_spacial = im.item(i,j),im.item(i,j+1),im.item(i+1,j),im.item(i+1,j+1)
 			array_spacial_past = im_past.item(i,j),im_past.item(i,j+1),im_past.item(i+1,j),im_past.item(i+1,j+1)
 
@@ -136,5 +120,15 @@ def find_values3(im, im_past):
 			array_iy[i][j] = ((array_spacial[2] - array_spacial[0]) + (array_spacial[3] - array_spacial[1]) + (array_spacial_past[2] - array_spacial_past[0]) + (array_spacial_past[3] - array_spacial_past[1]))/4
 			array_it[i][j] = ((array_spacial[0] - array_spacial_past[0])+(array_spacial[1] - array_spacial_past[1])+(array_spacial[2] - array_spacial_past[2])+(array_spacial[3] - array_spacial_past[3]))/4
 	return array_ix, array_iy, array_it
+
+def regions(im, regions_to_detect, region_size):
+	region_list = np.empty((len(regions_to_detect),region_size[0],region_size[1]),np.uint8)
+
+	for i in range(0, len(regions_to_detect)):
+		region_individual = im[regions_to_detect[i][0]:regions_to_detect[i][0] + region_size[0], regions_to_detect[i][1]:regions_to_detect[i][1] + region_size[1]]
+		region_list[i] = region_individual
+
+	print region_list
+	#return region_list
 
 main()
