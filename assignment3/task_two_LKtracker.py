6e51f72bb46a0fb53193472d198bb24ecb7c95f6
{{git.name}} <alex1412108@gmail.com>
2014-12-15 06:29:17 +0000

    code calculates vectors and displays them using lines, may need accuracy checking

--- assignment3/task_two_LKtracker.py
+++ assignment3/task_two_LKtracker.py
@@ -13,13 +13,23 @@ def main():
 	#video_source = cv2.VideoCapture(0)
 	#ret = video_source.set(3,1920) #set width
 	#ret = video_source.set(4,1080) #set height
-	regions_to_detect = [[200,200],[100,300],[50,50]]
-	region_size = [3,3]
+
 
 	im = setup(video_source)
-	im = cleanup(im)
-	region_list = regions(im, regions_to_detect, region_size)
 
+	im = cleanup(im)
+
+	rows, cols = im.shape
+
+	regions_to_detect = []
+	for i in range(10, cols-5, cols/10):
+	 	for j in range(10, rows-5, rows/10):
+	 		regions_to_detect.append([i,j])
+
+	region_size = [3,3]
+
+	region_list = regions(im, regions_to_detect, region_size)
+	
 	while True:
 		region_list_past = region_list.copy()
 
@@ -31,22 +41,25 @@ def main():
 
 		vector_list = compute_lk(results_list)
 
-		#for i in range(0,len(results_list)):
+		#print vector_list[0][0][0]
+
+		for i in range(0,len(vector_list)):
 			#cv2.imshow(str(i) + 'ix', results_list[i][0])
 			#cv2.imshow(str(i) + 'iy', results_list[i][1])
 			#cv2.imshow(str(i) + 'it', results_list[i][2])
+			#cv2.line(im,(0,regions_to_detect[i][0] + vector_list[i][0][0]),(511,511),(255,0,0),5)
+			cv2.line(im, (regions_to_detect[i][0], regions_to_detect[i][1]), (regions_to_detect[i][0] + vector_list[i][0][0], regions_to_detect[i][1] + vector_list[i][1][0]),(255,0,0))
 
-		#cv2.imshow('image', im)
-		#cv2.imshow('image past', im_past)
+
+		cv2.imshow('image', im)
+		cv2.imshow('image past', im_past)
 		#cv2.imshow('ix', array_ix)
 		#cv2.imshow('iy', array_iy)
 		#cv2.imshow('it', array_it)
-		cv2.waitKey(0)
-		exit(0)
-		key = cv2.waitKey(10)
+
+		key = cv2.waitKey(20)
 		if key > 0:
 			if key==1048603: #escape key
-				print region_list
 				exit(0)
 		# 		if key==1048608: #space key
 		# 			t = 2
@@ -62,6 +75,9 @@ def cleanup(im):
 def running_cleanup(video_source, im):
 	im_past_gray = im.copy()
 	im = video_source.read()[1]
+	if im == None:
+		print "end of video"
+		exit(0)
 	im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 	return im_gray, im_past_gray
 
@@ -135,15 +151,14 @@ def find_values3(im, im_past):
 
 def regions(im, regions_to_detect, region_size):
 	region_list = np.empty((len(regions_to_detect),region_size[0],region_size[1]),np.uint8)
-
 	for i in range(0, len(regions_to_detect)):
-		region_individual = im[regions_to_detect[i][0]:regions_to_detect[i][0] + region_size[0], regions_to_detect[i][1]:regions_to_detect[i][1] + region_size[1]]
+		region_individual = im[regions_to_detect[i][1]:regions_to_detect[i][1] + region_size[1], regions_to_detect[i][0]:regions_to_detect[i][0] + region_size[0]] # format is y1:y2:x1:x2
 		region_list[i] = region_individual
 
 	return region_list
 
 def compute_regions(region_list, region_list_past):
-	results_list = np.empty((3,len(region_list),len(region_list[0])-1,len(region_list[0][0])-1),np.uint8)
+	results_list = np.empty((len(region_list),3,len(region_list[0])-1,len(region_list[0][0])-1),np.uint8)
 	for i in range(0,len(region_list)):
 		array_ix, array_iy, array_it = find_values3(region_list[i], region_list_past[i])
 		results_list[i] = [array_ix, array_iy, array_it]
@@ -151,22 +166,38 @@ def compute_regions(region_list, region_list_past):
 	return results_list
 
 def compute_lk(results_list):
-	print results_list
-	vector_list = np.empty((len(results_list),1,2),np.uint8)
+	vector_list = np.empty((len(results_list),2,1),np.uint8)
 	for i in range(0,len(results_list)):
-		ix = results_list[i][0]
-		iy = results_list[i][1]
-		it = results_list[i][2]
+		ix = np.reshape(results_list[i][0], 4)
+		iy = np.reshape(results_list[i][1], 4)
+		it = np.reshape(results_list[i][2], 4)
 
-		A = np.array([[np.dot(ix, ix), np.dot(ix, iy)], [np.dot(ix, iy), np.dot(iy, iy)]])
-		b = [[-1 * np.dot(ix, it)], [-1 * np.dot(iy, it)]]
+		A = np.array([[(ix[0]**2 + ix[1]**2 + ix[2]**2 + ix[3]**2)/4, (ix[0]*iy[0] + ix[1]*iy[1] + ix[2]*iy[2] + ix[3]*iy[3])/4], [(ix[0]*iy[0] + ix[1]*iy[1] + ix[2]*iy[2] + ix[3]*iy[3])/4, (iy[0]**2 + iy[1]**2 + iy[2]**2 + iy[3]**2)/4]])
+		b = np.array([[-1 * (ix[0]*it[0] + ix[1]*it[1] + ix[2]*it[2] + ix[3]*it[3])/4], [-1 * (iy[0]*it[0] + iy[1]*it[1] + iy[2]*it[2] + iy[3]*it[3])/4]])
 		#calculate inverse of A
-		#print A
-		Ainv = np.linalg.inv(A)
-		#calculate product of inverse of A and b
-		v = np.dot(Ainv, b)
+		try:
+			#calculate product of inverse of A and b
+			Ainv = np.linalg.inv(A)
+			v = np.dot(Ainv, b)
+		except:
+			#if a singularity occurs this zeroes the vector, the print statement below is used for being alerted of singularities
+			#print "singularity at coordinate pair " + str(i+1)
+			v = np.array([[0],[0]])
+
+		print v[0]
+
+		#clean out extreme to reduce noise.
+		if v[0] > 250:
+			v = np.array([[0],[0]])
+
+		elif v[1] > 250:
+			v = np.array([[0],[0]])
 
 		vector_list[i] = v
-	#print vector_list
+
+	return vector_list
+
+#def block_matrix_inverse(matrix):
+
 
 main()
